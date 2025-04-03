c0a45d1087ba03076b56ad25a69e31e54efd1a3c
nv-edwli <edwli@nvidia.com>
2025-04-03 21:43:59 +0000

    Modified workbench/init.sh
    - Modified files: workbench/init.sh

--- workbench/init.sh
+++ workbench/init.sh
@@ -1,11 +1,11 @@
-# Adjust perms for locally downloaded content
-chmod -R 775 demo
-chmod -R 775 routers
-
 # Download Routers Locally
 make download
 
 # Configure API Key for Build Endpoints models
 python3 workbench/init.py
+
+# Adjust new content to avoid perm issues
+chmod -R 775 demo
+chmod -R 775 routers
 
 exit 0

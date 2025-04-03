5c4f8ce5222396a5da5f7b8a558a12bbee9bdf16
nv-edwli <144396897+nv-edwli@users.noreply.github.com>
2025-04-03 17:25:15 -0500

    Update init.sh

--- workbench/init.sh
+++ workbench/init.sh
@@ -1,13 +1,7 @@
-export PYTHONDONTWRITEBYTECODE=1
-
 # Download Routers Locally
 PYTHONDONTWRITEBYTECODE=1 make download
 
 # Configure API Key for Build Endpoints models
 python3 workbench/init.py
-
-# Adjust new content to avoid perm issues
-chmod -R 775 demo
-chmod -R 775 routers
 
 exit 0
